5014bcd375e067a810f62494f47965408772dc3f
Viesta98k <yk.chiu.07@gmail.com>
2023-04-28 13:48:48 +0800

    a

---
+++ src/vocabulary.cpp
@@ -0,0 +1,22 @@
+#include<string>
+#include<vector>
+#include"csv.hpp"
+#include"vocabulary.hpp"
+
+using namespace std;
+
+double vocabulary::initDifficultyOf(string vocab){
+    int count;
+    bool prev_letter=false;
+    for(int i=0;i<vocab.size();i++){
+        
+    }
+}
+
+bool vocabulary::isVowel(char input){
+    return input=='a'||input=='e'||input=='i'||input=='o'||input=='u'||input=='y';
+}
+
+int vocabulary::countSyllable(string vocab){
+
+}
